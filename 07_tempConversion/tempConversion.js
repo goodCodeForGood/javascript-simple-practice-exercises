cbb6834775437ad6a8bfe2a9a229453583d31f25
goodCodeForGood <sahitihibane6@gmail.com>
2024-11-16 00:36:03 -0800

    Add correct code to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f_temp) {
+   let c_temp = (f_temp - 32) * (5/9);
+   // to round up to 1 decimal place
+   let rounded_c_temp = Math.round(c_temp * 10) / 10; 
+   return rounded_c_temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c_temp) {
+  let f_temp = (c_temp * (9/5)) + 32;
+  // to round up to 1 decimal place
+  let rounded_f_temp = Math.round(f_temp * 10) / 10; 
+  return rounded_f_temp;
 };
 
 // Do not edit below this line
