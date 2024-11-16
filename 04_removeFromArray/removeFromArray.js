22ec106f721d61159b888100237f4b4c6d5102e1
goodCodeForGood <sahitihibane6@gmail.com>
2024-11-15 23:24:46 -0800

    Add correct code to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+// Use "..." (rest syntax) to put all possible args (even multiple ones) into "args" array
+const removeFromArray = function (array, ...args) {
+  let new_array = [];
+
+  array.forEach((element) => {
+    // if the element is not included in the array of args, add it to new array
+    // otherwise, that element is not added to new array
+    if (!args.includes(element)) {
+      new_array.push(element);
+    }
+  });
+  return new_array;
 };
 
 // Do not edit below this line
